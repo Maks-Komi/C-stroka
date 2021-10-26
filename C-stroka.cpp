c5a371c5938dcf7c7544f58e4380ee45cd6f6c7a
Komi_op87@stud.msk.itstep.org <Komi_op87@stud.msk.itstep.org>
2021-10-26 22:11:09 +0300

    исправлено

--- C-stroka.cpp
+++ C-stroka.cpp
@@ -8,27 +8,34 @@ using namespace std;
    - Откройте файл.
    - Прочитайте его в C-строку (char *).
    - Используя функцию сравнения строк, найдите в нем подстроку.
-
+   
    Пример использования:
    search.exe myfile.txt mysubstr
    Symbol 28*/
 
 int main(int argc, char* argv[]) {
-	string filename;
-	string str;
-	char substr[50];
-	const char* c_str;
-	istream& fin = cin;
-	getline (fin, filename);
-	cin >> substr;
-	ifstream file;
-	file.open(filename);
-	file >> str;
-	c_str = str.c_str();
-	printf(c_str);
-	const char* p;
-	p = strstr(c_str, substr);
-	cout << endl;
-	printf("Simbol: %d\n", p - c_str + 1);
-	file.close();
+	/* Получаем название файла из командной строки 
+	  - открывем этот файл
+	  - считываем из него строку
+	  - ищем в ней подстроку
+	  - закрывем файл*/
+	if (argc == 3) {
+		string str, sub_str;
+		ifstream file;
+		file.open(argv[1]);
+		const char* c_str;
+		const char* c_substr;
+		//filename(argv[1]);
+		while(file >> str);
+		cout << str.find(sub_str);
+		c_str = str.c_str();
+		c_substr = sub_str.c_str();
+		printf(c_str);
+		const char* p;
+		p = strstr(c_str, c_substr);
+		cout << endl;
+		printf("Simbol: %d\n", p - c_str + 1);
+		//file.close();
+	}
+	return 0;
 }
